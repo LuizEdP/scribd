b4a960a1d4737903de3eb27ce49d516dd65d905f
LuizEdP <luiz.eduardo@mrpbrasil.com>
2015-06-07 14:38:37 -0300

    Create antilogin.js

---
+++ antilogin.js
@@ -0,0 +1,7 @@
+javascript:(function(){
+$('.between_page_ads').remove();
+$('.text_layer').attr('style','z-index: 2;');
+$('div[id^="page_missing_explanation"]').remove();
+$('.autogen_class_views_read2_page_blur_promo.autogen_class_widgets_base').remove();
+$('.outer_page.only_ie6_border.blurred_page').attr('class','outer_page only_ie6_border   not_visible');
+}());
