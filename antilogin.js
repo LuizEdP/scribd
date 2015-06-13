58ddab1552e028d3b44f7a504a42354bb8b6c328
LuizEdP <luiz.eduardo@mrpbrasil.com>
2015-06-13 18:31:00 -0300

    Update antilogin.js

--- antilogin.js
+++ antilogin.js
@@ -4,4 +4,5 @@ $('.text_layer').attr('style','z-index: 2;');
 $('div[id^="page_missing_explanation"]').remove();
 $('.autogen_class_views_read2_page_blur_promo.autogen_class_widgets_base').remove();
 $('.outer_page.only_ie6_border.blurred_page').attr('class','outer_page only_ie6_border   not_visible');
+$('.seo_banner_spacer').remove();
 }());
